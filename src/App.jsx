d2a119112c815f0a282a4a62aea4f3a3e8521611
Francisco Xavier <xavieraraque@gmail.com>
2021-12-03 17:32:00 +0100

    Add NavBar

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+import { Container } from "@chakra-ui/react";
+import Header from "./components/Header";
+
+export default function App() {
+    return (
+        <Fragment>
+            <Header />
+            <Container maxW='container.lg'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia impedit animi error qui a voluptate accusamus iusto amet. Ut repellat rerum dignissimos exercitationem laudantium consectetur similique, aut expedita obcaecati.</p>
+            </Container>
+        </Fragment>
+    );
+}
