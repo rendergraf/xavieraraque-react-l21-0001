ccc977dbd31e25f40b2220f04ac459ff175822d2
Francisco Xavier <xavieraraque@gmail.com>
2021-12-11 13:43:22 +0100

    Add Suspense 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Fragment, Suspense } from 'react';
-import { Spinner } from '@chakra-ui/react'
-import Header from "./components/Header";
-import Hero from './components/Hero';
+import React, { Fragment, Suspense } from 'react';
+import { Center, Spinner } from '@chakra-ui/react'
+/* import Header from "./components/Header"; */
+/* import Hero from './components/Hero'; */
 import About from './components/About';
 import OurServices from './components/OurServices.tsx';
 import Portfolio from './components/Portfolio';
@@ -9,10 +9,22 @@ import Contact from './components/Contact';
 import ThisWebsite from './components/ThisWebsite';
 import Footer from './components/Footer'
 
+const Header = React.lazy(() => import("./components/Header"));
+const Hero = React.lazy(() => import("./components/Hero"));
+
 export default function App() {
     return (
-        <Fragment>
-            <Suspense fallback={<Spinner />}>
+         <Fragment>
+            <Suspense fallback={
+                <Center bg='black' h='100vh' color='white'>
+                    <Spinner
+                    thickness='2px'
+                    speed='0.65s'
+                    emptyColor='gray.900'
+                    color='cyan.600'
+                    size='xl'/>
+                </Center>
+            }>
                 <Header />
                 <Hero />
             </Suspense>
